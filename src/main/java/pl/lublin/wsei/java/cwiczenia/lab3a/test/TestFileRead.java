89d55e2301d2af3937e40d43ddebe005bbb94cff
jpawlowski412 <jpawlowski412@gmail.com>
2022-05-24 11:55:08 +0200

    Utworzenie klasy TestFileRead

---
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab3a/test/TestFileRead.java
@@ -0,0 +1,23 @@
+package pl.lublin.wsei.java.cwiczenia.lab3a.test;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class TestFileRead {
+
+    public static void main(String[] args) throws java.io.IOException{
+
+        try{
+            String contents = new String(Files.readAllBytes(Paths.get("gusInfoGraphic.xml")));
+            System.out.println("Zawartość pliku gusInfoGraphic.xml");
+            System.out.print(contents);
+        }
+
+        catch (IOException e){
+
+            System.out.println("Błąd wczytywania pliku gusInfoGraphic.xml =>"+ e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+    }
+}
