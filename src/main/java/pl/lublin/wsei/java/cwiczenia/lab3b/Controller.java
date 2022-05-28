03b88c6aa0c65fe5fa910cdf801a75cd6dc288fd
jpawlowski412 <jpawlowski412@gmail.com>
2022-05-28 11:28:59 +0200

    Dodanie miniaturek i adresu strony, modyfikacja interfejsu

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab3b/Controller.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab3b/Controller.java
@@ -1,5 +1,7 @@
 package pl.lublin.wsei.java.cwiczenia.lab3b;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -7,12 +9,22 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import java.io.File;
+import javafx.application.HostServices;
+import javafx.stage.Stage;
 
 public class Controller {
     public Label lbFile;
     public Button mainButton;
+    public TextField txtAdresStrony;
+    public Button btnPrzejdzDoStrony;
+    public Button btnPokazInfografike;
+    public ImageView imgMiniaturka;
+    private Infografika selInfografika;
 
 
     FileChooser fileChooser = new FileChooser();
@@ -25,6 +37,24 @@ public class Controller {
     @FXML
     public void initialize(){
         fileChooser.getExtensionFilters().add(xmlFilter);
+        lstInfografiki.getSelectionModel().selectedIndexProperty().addListener(
+                new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> observableValue, Number old_val, Number new_val) {
+                        int index = new_val.intValue();
+                        if (index!= -1) {
+                            selInfografika = igList.infografiki.get(index);
+                            txtAdresStrony.setText(selInfografika.adresStrony);
+                            Image image = new Image(selInfografika.adresGrafiki);
+                            imgMiniaturka.setImage(image);
+                        }
+                        else
+                            txtAdresStrony.setText("");
+                            imgMiniaturka.setImage(null);
+                            selInfografika = null;
+                    }
+                }
+        );
     }
     public void btnOpenFileAction(ActionEvent actionEvent) {
         File file = fileChooser.showOpenDialog(null);
@@ -36,6 +66,22 @@ public class Controller {
         }
         else {
             lbFile.setText("Proszę wczytać plik ...");
+
         }
     }
+    public void btnZaladujStrone(ActionEvent actionEvent) {
+        if (selInfografika != null)
+            hostServices.showDocument(selInfografika.adresStrony);
+    }
+    private Stage stage;
+    private HostServices hostServices;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+    public void setHostServices(HostServices hostServices) {
+        this.hostServices = hostServices;
+    }
+
+
 }
--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab3b/Main.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab3b/Main.java
@@ -11,7 +11,12 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("gusInfoGraphic.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("gusInfoGraphic.fxml"));
+        Parent root =loader.load();
+        Controller controller = loader.getController();
+        controller.setHostServices(this.getHostServices());
+        controller.setStage(primaryStage);
+
         primaryStage.setTitle("gusInfoGraphic");
         primaryStage.setScene(new Scene(root, 800, 700));
         primaryStage.show();
